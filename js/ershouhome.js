ee5f05a831c894181101c275fbdbd027823efb7c
lancela <1690319247@qq.com>
2019-08-31 11:32:37 +0800

    commitone

---
+++ js/ershouhome.js
@@ -0,0 +1,111 @@
+
+
+function button21click(){
+	var button21 = document.getElementById("button21");
+	var button22 = document.getElementById("button22");
+	var div21 = document.getElementById("div21");
+	var div22 = document.getElementById("div22");
+	div21.style.display="block";
+	div22.style.display="none";
+	}
+
+function button22click(){
+	var button21 = document.getElementById("button21");
+	var button22 = document.getElementById("button22");
+	var div21 = document.getElementById("div21");
+	var div22 = document.getElementById("div22");
+	div21.style.display="none";
+	div22.style.display="block";
+	}
+
+function button1click(){
+	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var button5 = document.getElementById("button5");
+	var div1 = document.getElementById("div1");
+	var div2 = document.getElementById("div2");
+	var div3 = document.getElementById("div3");
+	var div4 = document.getElementById("div4");
+	var div5 = document.getElementById("div5");
+	div1.style.display="block";
+	div2.style.display="none";
+	div3.style.display="none";
+	div4.style.display="none";
+	div5.style.display="none";
+	}
+function button2click(){
+	var div3 = document.getElementById("div3");
+	var div4 = document.getElementById("div4");
+	var div5 = document.getElementById("div5");
+	div1.style.display="none";
+	div2.style.display="block";
+	div3.style.display="none";
+	div4.style.display="none";
+	div5.style.display="none";
+	}
+
+
+	
+function button3click(){
+	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var button5 = document.getElementById("button5");
+	var div1 = document.getElementById("div1");
+	var div2 = document.getElementById("div2");
+	var div3 = document.getElementById("div3");
+	var div4 = document.getElementById("div4");
+	var div5 = document.getElementById("div5");
+	div1.style.display="none";
+	div2.style.display="none";
+	div3.style.display="block";
+	div4.style.display="none";
+	div5.style.display="none";
+	}	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var button5 = document.getElementById("button5");
+	var div1 = document.getElementById("div1");
+	var div2 = document.getElementById("div2");
+
+	
+function button4click(){
+	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var button5 = document.getElementById("button5");
+	var div1 = document.getElementById("div1");
+	var div2 = document.getElementById("div2");
+	var div3 = document.getElementById("div3");
+	var div4 = document.getElementById("div4");
+	var div5 = document.getElementById("div5");
+	div1.style.display="none";
+	div2.style.display="none";
+	div3.style.display="none";
+	div4.style.display="block";
+	div5.style.display="none";
+	}
+
+	
+function button5click(){
+	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var button5 = document.getElementById("button5");
+	var div1 = document.getElementById("div1");
+	var div2 = document.getElementById("div2");
+	var div3 = document.getElementById("div3");
+	var div4 = document.getElementById("div4");
+	var div5 = document.getElementById("div5");
+	div1.style.display="none";
+	div2.style.display="none";
+	div3.style.display="none";
+	div4.style.display="none";
+	div5.style.display="block";
+	}
---
+++ js/index.js
@@ -0,0 +1,142 @@
+function closeadv(){
+	var adv =document.getElementById("adv");
+	adv.style.display="none";
+
+}
+
+function clickzhi(){
+	var zhinan = document.getElementById("zhinan");
+	zhinan.style.display="block";
+}
+function overzhi(){
+	var zhinan = document.getElementById("zhinan");
+	zhinan.style.display="block";
+}
+function leavezhi(){
+	var zhinan = document.getElementById("zhinan");
+	zhinan.style.display="none";
+}
+
+function sharchtwohouse(){
+	var itran = document.getElementById("triangle");
+	var input = document.getElementById("input");
+	itran.style.left="250px";
+	input.placeholder= "请输入区域 商圈进行找房~";
+}
+function sharchnewhouse(){
+	var itran = document.getElementById("triangle");
+	var input = document.getElementById("input");
+	itran.style.left="330px";
+	input.placeholder= "请输入楼盘名称进行找房~";
+
+}
+function sharchrenthouse(){
+	var itran = document.getElementById("triangle");
+	var input = document.getElementById("input");
+	itran.style.left="430px";
+	input.placeholder= "请输入区域 商圈进行找房~";
+}
+function sharchxaioqu(){
+	var itran = document.getElementById("triangle");
+	var input = document.getElementById("input");
+	input.placeholder= "请输入小区名称开始寻找小区~";
+	itran.style.left="520px";
+}
+function sharchanswer(){
+	var itran = document.getElementById("triangle");
+	var input = document.getElementById("input");
+	itran.style.left="600px";
+	input.placeholder= "房产知识有疑问?来搜搜吧~";
+}
+
+function button1click(){
+	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var button5 = document.getElementById("button5");
+	var div1 = document.getElementById("div1");
+	var div2 = document.getElementById("div2");
+	var div3 = document.getElementById("div3");
+	var div4 = document.getElementById("div4");
+	var div5 = document.getElementById("div5");
+	div1.style.display="block";
+	div2.style.display="none";
+	div3.style.display="none";
+	div4.style.display="none";
+	div5.style.display="none";
+	}
+function button2click(){
+	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var button5 = document.getElementById("button5");
+	var div1 = document.getElementById("div1");
+	var div2 = document.getElementById("div2");
+	var div3 = document.getElementById("div3");
+	var div4 = document.getElementById("div4");
+	var div5 = document.getElementById("div5");
+	div1.style.display="none";
+	div2.style.display="block";
+	div3.style.display="none";
+	div4.style.display="none";
+	div5.style.display="none";
+	}
+
+
+	
+function button3click(){
+	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var button5 = document.getElementById("button5");
+	var div1 = document.getElementById("div1");
+	var div2 = document.getElementById("div2");
+	var div3 = document.getElementById("div3");
+	var div4 = document.getElementById("div4");
+	var div5 = document.getElementById("div5");
+	div1.style.display="none";
+	div2.style.display="none";
+	div3.style.display="block";
+	div4.style.display="none";
+	div5.style.display="none";
+	}
+	
+function button4click(){
+	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var button5 = document.getElementById("button5");
+	var div1 = document.getElementById("div1");
+	var div2 = document.getElementById("div2");
+	var div3 = document.getElementById("div3");
+	var div4 = document.getElementById("div4");
+	var div5 = document.getElementById("div5");
+	div1.style.display="none";
+	div2.style.display="none";
+	div3.style.display="none";
+	div4.style.display="block";
+	div5.style.display="none";
+	}
+
+	
+function button5click(){
+	var button1 = document.getElementById("button1");
+	var button2 = document.getElementById("button2");
+	var button3 = document.getElementById("button3");
+	var button4 = document.getElementById("button4");
+	var button5 = document.getElementById("button5");
+	var div1 = document.getElementById("div1");
+	var div2 = document.getElementById("div2");
+	var div3 = document.getElementById("div3");
+	var div4 = document.getElementById("div4");
+	var div5 = document.getElementById("div5");
+	div1.style.display="none";
+	div2.style.display="none";
+	div3.style.display="none";
+	div4.style.display="none";
+	div5.style.display="block";
+	}
